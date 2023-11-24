11114b7428d9d746cd409c9be0d9a8e979b1e6f6
gp-1108 <pietrogirotto00@gmail.com>
2023-11-24 18:41:20 +0100

    refined the logic and ready for export

--- retrieve_free_slots.js
+++ retrieve_free_slots.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
 const axios = require('axios');
+const zlib = require('zlib'); // use zlib to decompress
 const Room = require('./Room.js');
 
 /**
@@ -13,6 +13,7 @@ const Room = require('./Room.js');
 async function getData(date, locationCode) {
   // eslint-disable-next-line max-len
   const data = `form-type=rooms&view=rooms&include=rooms&aula=&sede=${sede}&date=${giorno}&_lang=en&list=&week_grid_type=-1&ar_codes_=&ar_select_=&col_cells=0&empty_box=0&only_grid=0&highlighted_date=0&all_events=0&_lang=en`;
+
 
   const config = {
     method: 'post',
@@ -32,30 +33,42 @@ async function getData(date, locationCode) {
       'Pragma': 'no-cache',
       'Cache-Control': 'no-cache',
     },
+    responseType: 'arraybuffer', // Axios is not able to decompress the response
     data: data,
   };
 
   try {
     const response = await axios(config);
-    return response.data;
+    const decompressedData = zlib.gunzipSync(response.data).toString('utf-8');
+    return JSON.parse(decompressedData);
   } catch (err) {
     console.log(err);
     return null;
   }
 }
 
-// Get current date in format dd-mm-yyyy
-let giorno = new Date().toLocaleDateString('it-IT', {
-  day: '2-digit',
-  month: '2-digit',
-  year: 'numeric',
-}).replace(/\//g, '-');
-const sede = '00280'; // Polo Gradenigo
-
-// giorno = '31-03-2023';
 
 
-getData(giorno, sede).then((data) => {
+/**
+ * This function retrieves all available slots for a given date and location
+ * per each room.
+ * @param {string} date The date in the format dd-mm-yyyy.
+ * @param {string} locationCode The location code for the building complex.
+ */
+async function retrieveSlots(date, locationCode) {
+  const data = await getData(date, locationCode);
+  /*
+    Convert the data to a more readable format
+    the new object will have the following structure:
+    {
+      timestamp_from: <timestamp>,
+      timestamp_to: <timestamp>,
+      CodiceAula: <room code>,
+      NomeAula: <room name>,
+      name: <lesson name>,
+      orario: <time in format hh:mm - hh:mm>,
+    }
+  */
   const events = data['events'].map((eventRoom) => {
     return {
       timestamp_from: eventRoom['timestamp_from'],
@@ -67,13 +80,7 @@ getData(giorno, sede).then((data) => {
     };
   });
 
-  fs.writeFileSync('./data.json', JSON.stringify(events, null, 2), (err) => {
-    if (err) {
-      console.log(err);
-    }
-  });
-
-  // Testing
+  // Create a map of rooms
   const map = new Map();
   // eslint-disable-next-line guard-for-in
   for (lesson in events) {
@@ -85,13 +92,35 @@ getData(giorno, sede).then((data) => {
         .addLesson(events[lesson].timestamp_from, events[lesson].timestamp_to);
   }
 
+  return map;
+}
 
-  fs.writeFileSync('available.json',
-      JSON.stringify(Object.fromEntries(map), null, 2), (err) => {
-        if (err) {
-          console.log(err);
-        }
-      });
+module.exports = retrieveSlots;
 
-  return events;
-});
+/*
+  An example usage of the output of the function:
+
+  // Get current date in format dd-mm-yyyy
+  const giorno = new Date().toLocaleDateString('it-IT', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  }).replace(/\//g, '-');
+  const sede = '00280'; // Polo Gradenigo
+
+  const rooms = await retrieveSlots(giorno, sede);
+  for (const room of rooms.values()) {
+    const hours = room.getFreeHours();
+    for (let i = 0; i < hours.length-1; i+= 2) {
+      // Multiply by 1000 to get milliseconds
+      const initDate = new Date(hours[i]*1000);
+      const endDate = new Date(hours[i+1]*1000);
+
+      const hh_mm_start = `${initDate.getHours().toString().padStart(2, '0')}:${initDate.getMinutes().toString().padStart(2, '0')}`;
+      const hh_mm_end = `${endDate.getHours().toString().padStart(2, '0')}:${endDate.getMinutes().toString().padStart(2, '0')}`;
+
+      console.log(
+          `Room ${room.roomName} free from ${hh_mm_start} to ${hh_mm_end}`);
+    }
+  }
+*/
